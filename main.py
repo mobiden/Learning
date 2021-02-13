2e6ead20fef6469b61d1497721f127141cfc90b5
mobiden <mobiden@users.noreply.github.com>
2021-02-13 21:40:29 +0300

    Список победителей

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#
+myfile = open('input.txt', 'r', encoding='utf-8')
+lines = myfile.readlines()
+valuepoblist = [0, 0, 0]
+sizepoblist = [0, 0, 0]
+for line in lines:
+    peo = list(line.split())
+    peo = (peo[0], peo[1], int(peo[2]), int(peo[3]))
+    if valuepoblist[peo[2] - 9] < peo[3]:
+        valuepoblist[peo[2] - 9] = peo[3]
+        sizepoblist[peo[2] - 9] = 1
+    elif valuepoblist[peo[2] - 9] == peo[3]:
+        sizepoblist[peo[2] - 9] += 1
+myfile.close()
+
+print(*sizepoblist)
---
+++ Сортировка победителей по списку.py
@@ -0,0 +1,11 @@
+# сортировка победителей по списку
+n = int(input())
+uch = []
+for x in range(n):
+    nuch = list(input().split())
+    nuch = [int(nuch[1]), nuch[0]]
+    uch.append(nuch)
+
+uch.sort(reverse=True)
+for x in range(n):
+    print(uch[x][1])
