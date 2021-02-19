d0586cf1c8d71b18b0cc45a1022d41a292f4c5c8
mobiden <mobiden@users.noreply.github.com>
2021-02-19 16:46:46 +0300

    Список победителей

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# количество победителей по классам
-myfile = open('input.txt', 'r', encoding='utf-8')
-lines = myfile.readlines()
-valuepoblist = [0, 0, 0]
-sizepoblist = [0, 0, 0]
-for line in lines:
-    peo = list(line.split())
-    peo = (peo[0], peo[1], int(peo[2]), int(peo[3]))
-    if valuepoblist[peo[2] - 9] < peo[3]:
-        valuepoblist[peo[2] - 9] = peo[3]
-        sizepoblist[peo[2] - 9] = 1
-    elif valuepoblist[peo[2] - 9] == peo[3]:
-        sizepoblist[peo[2] - 9] += 1
-myfile.close()
+myfile = open('input.txt', 'r', encoding='utf8')
+countries = {}
+n = int(myfile.readline())
+for nu in range(n):
+    line = myfile.readline()
+    coun = line[:line.find(' ')].strip()
+    city = line[line.find(' ') + 1:].split()
+    for ci in city:
+        countries[ci] = []
+        countries[ci].append(coun)
+m = int(myfile.readline())
+for nu in range(m):
+    line = myfile.readline().strip()
+    if line in countries:
+        print(*countries[line])
 
-print(*sizepoblist)
+myfile.close()
