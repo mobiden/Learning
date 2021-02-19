33e6844ed5f9f766176592643c59f1f1d9a0ada1
mobiden <mobiden@users.noreply.github.com>
2021-02-19 22:11:16 +0300

    dictionary, list and tuple

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
+myDict = {}
 myfile = open('input.txt', 'r', encoding='utf8')
-countries = {}
-n = int(myfile.readline())
-for nu in range(n):
-    line = myfile.readline()
-    coun = line[:line.find(' ')].strip()
-    city = line[line.find(' ') + 1:].split()
-    for ci in city:
-        countries[ci] = []
-        countries[ci].append(coun)
-m = int(myfile.readline())
-for nu in range(m):
-    line = myfile.readline().strip()
-    if line in countries:
-        print(*countries[line])
-
+mylist = []
+for line in myfile.readlines():
+    listname = line.split()
+    for word in listname:
+        myDict[word] = myDict.get(word, 0) + 1
 myfile.close()
+for word in myDict:
+    mylist.append((-myDict[word], word))
+for word in sorted(mylist):
+    print(word[1])
