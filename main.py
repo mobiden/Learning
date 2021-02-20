0438fd93ceb9e38037f35ee688eb8496e2e54fd2
mobiden <mobiden@users.noreply.github.com>
2021-02-20 15:26:30 +0300

    подсчет продаж и ассортимента по покупателям

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-myDict = {}
+# подсчет продаж и ассортимента по покупателям
+itemDict = {}
 myfile = open('input.txt', 'r', encoding='utf8')
-mylist = []
-partylist = []
-su = allsum = 0
-for line in myfile.readlines():
-    party = line[:line.rfind(' ')].strip()
-    partylist.append([0, party, 0, 0])
-    myDict[party] = int(line[line.rfind(' '):])
-    su += int(line[line.rfind(' '):])
+for line in myfile:
+    xline = list(line.split())
+    if xline[0] not in itemDict:
+        itemDict[xline[0]] = {}
+    if xline[1] not in itemDict[xline[0]]:
+        itemDict[xline[0]][xline[1]] = 0
+    itemDict[xline[0]][xline[1]] += int(xline[2])
 myfile.close()
-fz = su / 450
-for x in myDict:
-    vo = myDict[x]
-    for i in range(len(partylist)):
-        if partylist[i][1] == x:
-            partylist[i][0] = vo
-            partylist[i][2] = int(vo // fz)
-            partylist[i][3] = vo % fz
-            break
-        #    mylist.append([vo, x, int(vo // fz), vo % fz])
-    allsum += int(myDict[x] // fz)
-while 450 - allsum > 0:
-    for xp in sorted(partylist, key=lambda x: -x[3]):
-        xp[2] += 1
-        allsum += 1
-        if 450 - allsum == 0:
-            break
-for i in partylist:
-    print(i[1], i[2])
+for fam in sorted(itemDict):
+    print(fam + ':')
+    for i in sorted(itemDict[fam]):
+        print(i, itemDict[fam][i])
