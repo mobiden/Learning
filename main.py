790a7ea55ea0c9f4eef2283702090d6c62193342
mobiden <mobiden@users.noreply.github.com>
2021-02-24 12:03:26 +0300

    Romanovs' family tree

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-# проверка контрольной работы
-def checkUpper(word):
-    x = 0
-    for i in word:
-        if i.isupper():
-            x += 1
-            if x > 1:
-                break
-    return True if x == 1 else False
+# генеалогическое дерево
+def addchild (parent, tempdict, theTree):
+    s = 0
+    while len(tempdict) > 0:
+        if parent == tempdict[s][0]:
+            chi = {tempdict[s][1]: {}}
+            theTree.setdefault(parent, []).append(chi)
+            tempdict.remove(tempdict[s])
+        else:
+            s += 1
+            if s == len(tempdict):
+                for x in range(len(theTree[parent])):
+                    sdt = list(theTree[parent][x].items())[0][0]
+                    print(theTree[parent].keys())
+#                    parent = theTree[parent][x]
+                    print(str(theTree[parent]))
+                    parent = theTree[parent]
+                    theTree = addchild(parent, tempdict, theTree[parent])
 
 
-theDict = {}
-nuerr = 0
+tempdict = []
+theTree = {}
+parents = set()
+children = set()
 myfile = open('input.txt', 'r', encoding='utf8')
 n = int(myfile.readline())
-for i in range(n):
-    dword = str(myfile.readline().strip())
-    if dword.lower() not in theDict:
-        theDict[dword.lower()] = []
-    theDict[dword.lower()].append(dword)
+for i in range(n - 1):
+    x = myfile.readline().split()
+    parent, child = x[1].strip(), x[0].strip()
+    parents.add(parent)
+    children.add(child)
+    tempdict.append([parent, child])
+parents -= children
+parent = parents.pop()
+theTree = addchild(parent, tempdict, theTree)
 
-for line in myfile.readlines():
-    xline = line.split()
-    for word in xline:
-        if word.lower() not in theDict:
-            if not checkUpper(word):
-                nuerr += 1
-#            else:
-#               theDict[word.lower()] = word
-        else:
-            if word not in theDict[word.lower()]:
-                nuerr += 1
-print(nuerr)
+print(theTree)
+
+
+
 myfile.close()
