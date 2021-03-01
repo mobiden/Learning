aca0a76022c59542d67bbe78fe2ee0c76a170f8a
mobiden <mobiden@users.noreply.github.com>
2021-03-01 21:55:12 +0300

    функциональное программирование: XOR нескольких строк

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import sys
-
-print((any(map(lambda x: x == 0, map(int, sys.stdin.read().strip().split())))))
+from sys import stdin
+from functools import reduce
+print(*(map(lambda z: reduce(lambda x, y: (2 - int(x) - int(y)) % 2, z),
+            list(filter(lambda x: x[0] != ' ', zip(
+                *(stdin.read().strip().split('\n')[1:])))))))
