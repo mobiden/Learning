60048f0e79e8cec6f423c1d88d75193b24f15501
mobiden <mobiden@users.noreply.github.com>
2021-03-03 14:50:59 +0300

    функциональное программирование: ставки на тараканов

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from math import sqrt
-print(*filter(
-         lambda x:
-                 (all(
-          map(lambda y: x % y != 0,
-                     range(2, round(sqrt(x)) + 1)
-                         ))),
-                list(range(2, int(input()) + 1))))
+from itertools import permutations
+from functools import reduce
+
+line = open('input.txt', 'r', encoding='utf8').read().split()
+
+stvar = list(map(lambda x: reduce(lambda a, b: str(a) + str(b), x),
+                 (permutations(range(1, int(line[0]) + 1))))) # варианты забега
+
+
+print(stvar[list(filter(lambda x: x[1],
+    list(enumerate(map(lambda x: (all(map(lambda k:
+        (x.find(line[4 + k * 4: 6 + k * 4][0]) <= x.find(line[4 + k * 4: 6 + k * 4][1])) ^
+        (x.find(line[2 + k * 4: 4 + k * 4][0]) <= x.find(line[2 + k * 4: 4 + k * 4][1])),
+                  range(int(line[1]))))),
+             stvar)))))[0][0]]
+      if list(filter(lambda x: x[1],
+    list(enumerate(map(lambda x: (all(map(lambda k:
+        (x.find(line[4 + k * 4: 6 + k * 4][0]) <= x.find(line[4 + k * 4: 6 + k * 4][1])) ^
+        (x.find(line[2 + k * 4: 4 + k * 4][0]) <= x.find(line[2 + k * 4: 4 + k * 4][1])),
+                  range(int(line[1]))))),
+             stvar))))) != [] else '0')
+"""
+myansw = list(filter(lambda x: x[1],
+    list(enumerate(map(lambda x: (all(map(lambda k:
+        (x.find(line[4 + k * 4: 6 + k * 4][0]) <= x.find(line[4 + k * 4: 6 + k * 4][1])) ^
+        (x.find(line[2 + k * 4: 4 + k * 4][0]) <= x.find(line[2 + k * 4: 4 + k * 4][1])),
+                  range(int(line[1]))))),
+             stvar)))))
+"""
